c76181f83989d8f60f6eea06345661c86592a73f
@dniel0225 <marquezlink@hotmail.com>
2024-02-21 07:39:03 -0500

    Frontend Improvements

--- src/App.js
+++ src/App.js
@@ -1,77 +1,109 @@
-import { useState } from 'react';
-import { Toaster } from 'react-hot-toast';
-import toast from 'react-hot-toast';
-import './App.css';
-import axios from 'axios';
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [selectedFiles, setSelectedFiles] = useState(new File([], ''));
+  const [selectedFiles, setSelectedFiles] = useState(new File([], ""));
   const [loading, setLoading] = useState(false);
-
+  const [hash, setHash] = useState("");
+  const [txLink, setTxLink] = useState("");
   const handleFileChange = (event) => {
     const files = event.target.files;
     setSelectedFiles(files[0]);
   };
 
-  const handleSubmit = (event) => {
+  async function handleSubmit(event) {
     event.preventDefault();
     if (selectedFiles && selectedFiles.size === 0) {
-      toast.error('Por favor selecciona al menos un archivo');
+      toast.error("Please select at least one file");
       return;
     }
     setLoading(true);
 
     const formData = new FormData();
-    formData.append('file', selectedFiles);
-    const res = axios.post('http://localhost:3000/upload', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    setLoading(false);
-    toast.promise(res, {
-      loading: 'Subiendo archivos...',
-      success: 'Archivos subidos correctamente',
-      error: 'Hubo un error en la carga de tus archivos',
-    });
+    formData.append("file", selectedFiles);
 
-    // setSelectedFiles(new File([], ''));
+    // Display a loading toast
+    let toastId = toast.loading("Uploading files...");
+
+    try {
+      const response = await axios.post("http://localhost:3000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      // Assuming the API returns a JSON object with the IpfsHash
+      const ipfsHash = response.data.IpfsHash;
+      setTxLink(response.data.transactionHash);
+      setHash(ipfsHash);
+      // Dismiss the loading toast with a success message
+      toast.dismiss(toastId);
+      toast.success(`File uploaded succesfully`);
+    } catch (error) {
+      // Dismiss the loading toast with an error message
+      toast.dismiss(toastId);
+      toast.error("Error uploading the file");
+    } finally {
+      setLoading(false);
+    }
+  }
+  const handleRedirection = (hash) => {
+    // Define the base URL of the IPFS gateway
+    const baseUrl = "https://ipfs.io/ipfs/";
+    // Concatenate the base URL with the hash
+    const fullUrl = `${baseUrl}${hash}`;
+    // Open the full URL in a new tab
+    window.open(fullUrl, "_blank");
   };
-
+  const handleGoTo = (link) => {
+    // Open the full URL in a new tab
+    window.open(link, "_blank");
+  };
   return (
-    <div className="containerUpload">
-      <Toaster />
-      <h1 className="title">IPFS Upload Archives</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="inputFile" className="custom-input-file">
-          Seleccionar archivo
-        </label>
-        <input
-          disabled={loading}
-          type="file"
-          id="inputFile"
-          name="inputFile"
-          className="input-file-hidden"
-          onChange={handleFileChange}
-          multiple
-          accept=".jpg, .jpeg, .png, .mp4, .mp3"
-        />
-        <span id="fileName"></span>
-        <button disabled={loading} type="submit" className="button">
-          Enviar
-        </button>
-      </form>
-      {selectedFiles instanceof File && selectedFiles.size === 0 && (
-        <span>No has seleccionado ningun archivo</span>
-      )}
-      {selectedFiles instanceof File && selectedFiles.size > 0 && (
-        <span>Archivo seleccionado: {selectedFiles.name}</span>
-      )}
+    <>
+      <div className="containerUpload">
+        <Toaster />
+        <h1 className="title">IPFS Upload Archives</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="inputFile" className="custom-input-file">
+            Select file
+          </label>
+          <input
+            disabled={loading}
+            type="file"
+            id="inputFile"
+            name="inputFile"
+            className="input-file-hidden"
+            onChange={handleFileChange}
+            multiple
+            accept=".jpg, .jpeg, .png, .mp4, .mp3"
+          />
+          <span id="fileName"></span>
+          <button disabled={loading} type="submit" className="button">
+            Send
+          </button>
+        </form>
+        {selectedFiles instanceof File && selectedFiles.size === 0 && <span>You haven't select any file</span>}
+        {selectedFiles instanceof File && selectedFiles.size > 0 && <span>File selected: {selectedFiles.name}</span>}
 
-      <span className="compatibility">
-        Tipos de archivos compatibles: JPG, JPEG, PNG, MP3, MP4
-      </span>
-    </div>
+        <span className="compatibility">Compatible formats: JPG, JPEG, PNG, MP3, MP4</span>
+        <p className="hash">Resulting Hash: {hash}</p>
+        <button
+          className="contractButton"
+          onClick={() => handleGoTo("https://testnet.bscscan.com/address/0x7517FfAfF0F6C48c702eaA5eDEd5950c6fE2053b#readContract")}
+        >
+          See Smart Contract
+        </button>
+        <button className="linkButton" disabled={hash == ""} onClick={() => handleRedirection(hash)}>
+          See file on browser
+        </button>
+        <button className="txButton" disabled={hash == ""} onClick={() => handleGoTo("https://testnet.bscscan.com/tx/" + txLink)}>
+          See transaction on block explorer
+        </button>
+      </div>
+    </>
   );
 }
 
